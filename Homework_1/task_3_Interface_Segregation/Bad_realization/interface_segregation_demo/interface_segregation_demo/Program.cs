7af31057c1a9d3a0ef549f2910d65cb3338743ee
Александр Коляда <aleksander.kolyada@gmail.com>
2024-01-10 11:52:48 +0300

    Add project for bad implementation demo.

---
+++ Homework_1/task_3_Interface_Segregation/Bad_realization/interface_segregation_demo/interface_segregation_demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace interface_segregation_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
