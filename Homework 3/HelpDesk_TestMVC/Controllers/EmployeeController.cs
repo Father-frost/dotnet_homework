2c81ceb872fa1effd6d035845b5a059f308a35bf
Aleksander <aleksander.kolyada@gmail.com>
2024-03-11 17:31:37 +0300

    Create directory Homework3; copy Homework2 with EmployeeController to Homework3

---
+++ Homework 3/HelpDesk_TestMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,144 @@
+﻿using HelpDesk_BLL.Contracts.Identity;
+using HelpDesk_TestMVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HelpDesk_TestMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        static List<EmployeeViewModel> Employees = new List<EmployeeViewModel>();
+
+		private IEmployeeService _employeeService;
+		public EmployeeController(IEmployeeService employeeService)
+		{
+			_employeeService = employeeService;
+		}
+
+		// GET: EmployeeController
+		public ActionResult Index()
+        {
+            //return View(Employees);
+            return View(_employeeService.GetEmployees());
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return NotFound("Empty id supplied!");
+            }
+
+            try
+            {
+                var employee = Employees[id.Value];
+                return View(employee);
+            }
+            catch
+            {
+				return NotFound("No such record found!");
+			}
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        //Adding an Employee to List
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EmployeeViewModel employee)
+        {
+            try
+            {
+                employee.Id = Employees.Count;
+                Employees.Add(employee);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int? id)
+        {
+			if (!id.HasValue)
+			{
+				return NotFound("Empty id supplied!");
+			}
+
+            try
+            {
+                var employee = Employees[id.Value];
+                return View(employee);
+            }
+            catch
+            {
+				return NotFound("No such record found!");
+			}
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, EmployeeViewModel employee)
+        {
+            try
+            {
+                Employees[id] = employee;
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+		// GET: EmployeeController/Delete/5
+		[HttpGet]
+		public ActionResult Delete(int? id)
+		{
+			if (!id.HasValue)
+			{
+				return NotFound("Empty id supplied!");
+			}
+            try
+            {
+                var employee = Employees[id.Value];
+
+                return View(employee);
+            }
+            catch
+            {
+				return NotFound("No such record found!");
+			}
+		}
+
+		// POST: EmployeeController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int? id, EmployeeViewModel employee)
+		{
+            try
+            {
+                employee = Employees[id.Value];
+                Employees.Remove(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+				return NotFound("No such record found!");
+			}
+		}
+
+	}
+}
---
+++ Homework 3/HelpDesk_TestMVC/Models/EmployeeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using HelpDesk_TestMVC.Enums;
+
+namespace HelpDesk_TestMVC.Models
+{
+	public class EmployeeViewModel
+	{
+		public int Id { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+
+		public PostEnum Post { get; set; }
+		public string? Skills { get; set; }
+
+		public string FullName
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(LastName))
+				{
+					return FirstName;
+				}
+				return $"{FirstName} {LastName}";
+			}
+		}
+	}
+}
