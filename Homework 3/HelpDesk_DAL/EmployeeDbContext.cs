931699d4e068ee9325b50db414c3f4623a02c2e9
Aleksander <aleksander.kolyada@gmail.com>
2024-03-11 17:40:13 +0300

    Implement UnitOfWork and Repository patterns; add EmployeeDbContext in project.

---
+++ Homework 3/HelpDesk_DAL/EmployeeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HelpDesk_DAL
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+
+        }
+
+        // public DbSet<Feedback> Feedbacks;
+        // public DbSet<Order> Orders;
+        // ... - use ApplyConfigurationsFromAssembly instead and create configs for entities
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
