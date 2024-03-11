ae61521cb1d14ba60b565332f0c3ff5452a00bd4
Aleksander <aleksander.kolyada@gmail.com>
2024-03-11 17:48:48 +0300

    Create project for business logic layer; create IService marker interface and IEmployeeService with GetEmployees method.

---
+++ Homework 3/HelpDesk_BLL/Contracts/Identity/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using HelpDesk_DomainModel.Models.Identity;
+
+namespace HelpDesk_BLL.Contracts.Identity
+{
+    public interface IEmployeeService
+    {
+        List<Employee> GetEmployees();
+
+        Task <Employee> CreateEmployee(Employee employee);
+		Task <Employee> UpdateEmployeeContactData(Employee employee);
+		Task <Employee> DeleteEmployee(long employeeId);
+        Task<Employee> SetEmployeePost(long employeeId, PostEnum newPost);
+    }
+}
