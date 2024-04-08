9a193f650014b2e018f773e606b033f01e1ee9de
Aleksander <aleksander.kolyada@gmail.com>
2024-04-08 15:50:14 +0300

    Add required modifier to fields. Add error mesages for validation control.

--- Homework 5/HelpDesk_DomainModel/Models/Identity/Employee.cs
+++ Homework 5/HelpDesk_DomainModel/Models/Identity/Employee.cs
@@ -2,28 +2,32 @@
 
 namespace HelpDesk_DomainModel.Models.Identity
 {
-    public class Employee : Entity<long>
-    {
-        [MaxLength(100)]
-        public required string FirstName { get; set; }
-        [MaxLength(100)]
-        public string? LastName { get; set; }
-        [MaxLength(100)]
-        public required string Email { get; set; }
-        public required PostEnum Post { get; set; }
-        [MaxLength(200)]
-        public string? Skills { get; set; }
+	public class Employee : Entity<long>
+	{
+		[MaxLength(100)]
+		[MinLength(2, ErrorMessage = "The First Name is too short!")]
+		[Required(ErrorMessage = "First Name is required!")]
+		public required string FirstName { get; set; }
+		[MaxLength(100)]
+		[MinLength(2, ErrorMessage = "The Last Name is too short!")]
+		public string? LastName { get; set; }
+		[MaxLength(100)]
+		[Required(ErrorMessage = "Email is required!")]
+		public required string Email { get; set; }
+		public required PostEnum Post { get; set; }
+		[MaxLength(200)]
+		public string? Skills { get; set; }
 
-        public string FullName
-        {
-            get
-            {
-                if (string.IsNullOrWhiteSpace(LastName))
-                {
-                    return FirstName;
-                }
-                return $"{FirstName} {LastName}";
-            }
-        }
-    }
+		public string FullName
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(LastName))
+				{
+					return FirstName;
+				}
+				return $"{FirstName} {LastName}";
+			}
+		}
+	}
 }
