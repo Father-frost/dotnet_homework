b161dec9122d7d334171dda29abf72c235aac634
Александр Коляда <aleksander.kolyada@gmail.com>
2024-04-15 07:04:32 +0300

    Add model for city and country enum.

---
+++ Homework 6/Population/Models/CityViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Population.Models
+{
+	public class CityViewModel
+	{
+		public string? City { get; set; }
+		public CountryEnum Country { get; set; }
+		public int Population { get; set; }
+	}
+}
---
+++ Homework 6/Population/Models/CountryEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Population.Models
+{
+	public enum CountryEnum
+	{
+		China = 0,
+		Italy = 1,
+		Belarus = 2
+	}
+}
