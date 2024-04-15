b5e02ad25c1eae30d51e65b85c5d3c806f956e6c
Александр Коляда <aleksander.kolyada@gmail.com>
2024-04-15 07:05:45 +0300

    Create method for geting population in CountryController.

--- Homework 6/Population/Controllers/CountryController.cs
+++ Homework 6/Population/Controllers/CountryController.cs
@@ -6,6 +6,8 @@ namespace Population.Controllers
 {
 	public class CountryController : Controller
 	{
+		static List<CityViewModel> Cities = new List<CityViewModel>();
+
 		private readonly ILogger<CountryController> _logger;
 
 		public CountryController(ILogger<CountryController> logger)
@@ -27,6 +29,38 @@ namespace Population.Controllers
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		public IActionResult GetPopulation(string country, string cities)
+		{
+			string[] citiesArray  = cities.Split('/');
+								
+			var storedCity = new CityViewModel
+			{
+				City = "",	//All cities
+				Country = CountryEnum.China,
+				Population = 1412000000,
+			};
+
+			Cities.Add(storedCity);
+			
+			storedCity = new CityViewModel
+			{
+				City = "Rome",
+				Country = CountryEnum.Italy,
+				Population = 2873000,
+			};
+			Cities.Add(storedCity);
+
+			storedCity = new CityViewModel
+			{
+				City = "Venice",
+				Country = CountryEnum.Italy,
+				Population = 261900,
+			};
+			Cities.Add(storedCity);
+
+			return View();
 		}
 	}
 }
