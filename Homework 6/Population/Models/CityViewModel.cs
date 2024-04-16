ea965bd17a48d01181bc538b26d9114682c2ae2e
Aleksander <aleksander.kolyada@gmail.com>
2024-04-16 11:51:06 +0300

    Change Country type to simple string (instead of Enum).

--- Homework 6/Population/Models/CityViewModel.cs
+++ Homework 6/Population/Models/CityViewModel.cs
@@ -3,7 +3,7 @@
 	public class CityViewModel
 	{
 		public string? City { get; set; }
-		public CountryEnum Country { get; set; }
+		public string? Country { get; set; }
 		public int Population { get; set; }
 	}
 }
