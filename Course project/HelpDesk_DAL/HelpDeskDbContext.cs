1191ef5b36fb89145cc1d3f01c7be0c0704b83e7
Александр Коляда <aleksander.kolyada@gmail.com>
2024-05-06 14:56:03 +0300

    Copy DAL project with conficured UoW, Repository and DBContext.

---
+++ Course project/HelpDesk_DAL/HelpDeskDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HelpDesk_DomainModel.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpDesk_DAL
+{
+    public class HelpDeskDbContext : IdentityDbContext<User>
+    {
+        public HelpDeskDbContext(DbContextOptions<HelpDeskDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
---
+++ Course project/HelpDesk_DAL/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using HelpDesk_DAL.Repository;
+using HelpDesk_DomainModel.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace HelpDesk_DAL
+{
+    public interface IUnitOfWork
+    {
+        IDbContextTransaction BeginTransaction();
+
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class, IEntity;
+
+        Task<int> SaveChangesAsync();
+    }
+}
