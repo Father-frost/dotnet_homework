fd52d2290b02afc5e9506434c3ddb06046510e36
Александр Коляда <aleksander.kolyada@gmail.com>
2024-05-06 15:02:57 +0300

    Add viewmodels with enums.

---
+++ Course project/HelpDesk_MVC/Models/EmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HelpDesk_MVC.Enums;
+
+namespace HelpDesk_MVC.Models
+{
+	public class EmployeeViewModel
+	{
+		public long Id { get; set; }
+		public PostEnum Post { get; set; }
+		public string? Skills { get; set; }
+        public int? Experience { get; set; }
+        public string? Rating { get; set; }
+
+    }
+}
---
+++ Course project/HelpDesk_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelpDesk_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
